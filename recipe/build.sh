8e3ebe295b3f70cff3a7a11a306eced2c80f1382
v-turin <vladimir.turin@opencascade.com>
2018-11-23 17:01:45 +0300

    Linux build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 mkdir build && cd build
 
-cmake -G "Ninja"                            \
+#cmake -G "Ninja"                            \
+cmake                                       \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}        \
     -DCMAKE_PREFIX_PATH=${PREFIX}           \
     -DCMAKE_BUILD_TYPE=Release              \
     ..
 
-ninja
+#ninja
+make -j${CPU_COUNT}
 
 cp dem2mesh $PREFIX/bin
